eb39876b46486dc1656a0879e6d8b92a98964d84
Oleh <gend4016@gmail.com>
2024-03-18 10:50:29 +0200

    personal

--- app/personal/page.tsx
+++ app/personal/page.tsx
@@ -8,7 +8,7 @@ import {
   Stepper,
   Typography,
 } from "@mui/material";
-import { Metadata } from "next";
+// import { Metadata } from "next";
 import { ChangeEvent, useEffect, useState } from "react";
 import { styled } from "@mui/system";
 
@@ -18,11 +18,11 @@ import { PhoneNumberStep } from "@/components/personal/PhoneNumberStep";
 import { WalletAddressStep } from "@/components/personal/WalletAddressStep";
 import { PaymentHistory } from "@/components/personal/PaymentHistory";
 
-export const metadata: Metadata = {
-  title: "Personal | Bonus XXXCasinoGuru",
-  description:
-    "Embark on a thrilling journey through the diverse world of online casinos with our all-inclusive guide at Bonus XXXCasinoGuru. From the industry giants to hidden gems, our comprehensive guide reviews the most trustworthy and entertaining casinos in the market. Discover what sets each casino apart in terms of game offerings, customer service, bonuses, and security features. Additionally, navigate our curated list of top-rated online casinos to find the perfect match for your gaming preferences. Whether you're a novice player taking your first steps or a seasoned veteran, our guide equips you with everything you need for an enriching gaming experience.",
-};
+// export const metadata: Metadata = {
+//   title: "Personal | Bonus XXXCasinoGuru",
+//   description:
+//     "Embark on a thrilling journey through the diverse world of online casinos with our all-inclusive guide at Bonus XXXCasinoGuru. From the industry giants to hidden gems, our comprehensive guide reviews the most trustworthy and entertaining casinos in the market. Discover what sets each casino apart in terms of game offerings, customer service, bonuses, and security features. Additionally, navigate our curated list of top-rated online casinos to find the perfect match for your gaming preferences. Whether you're a novice player taking your first steps or a seasoned veteran, our guide equips you with everything you need for an enriching gaming experience.",
+// };
 
 
 const defaultCoin = "USDTTRC20";

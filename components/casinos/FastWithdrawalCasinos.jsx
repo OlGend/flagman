4bed286fb68d20956dbade9332b42b06ac5b2089
Oleh <gend4016@gmail.com>
2024-04-05 12:22:10 +0300

    a

--- components/casinos/FastWithdrawalCasinos.jsx
+++ components/casinos/FastWithdrawalCasinos.jsx
@@ -15,7 +15,7 @@ const FastWithdrawalCasinos = () => {
         <div className="flex flex-col basis-[60%]">
         
           <h1 className="text-white">{t("2024's Swiftest Payout Casino and Betting Platforms")}</h1>
-          <p className="text-white mt-5">{t("The most advantageous aspect of rapid payout casinos is their swift and unwavering dispensation of your earnings. We&#39;ve meticulously crafted this inventory to aid you in locating the supreme casinos offering expedited withdrawal processes, particularly when your earnings surge swiftly and substantially. Simply initiate a withdrawal and envisage the exhilarating possibilities for allocating those funds!")}</p>
+          <p className="text-white mt-5">{t("The most advantageous aspect of rapid payout casinos is their swift and unwavering dispensation of your earnings. We've meticulously crafted this inventory to aid you in locating the supreme casinos offering expedited withdrawal processes, particularly when your earnings surge swiftly and substantially. Simply initiate a withdrawal and envisage the exhilarating possibilities for allocating those funds!")}</p>
           {/* <Subscribe /> */}
         </div>
         <div className="basis-[40%] flex justify-center">

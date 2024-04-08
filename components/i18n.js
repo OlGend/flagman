c3e055dcd9a5253f9827a2e1c58c64fe92475d01
Oleh <gend4016@gmail.com>
2024-04-08 11:55:55 +0300

    a

--- components/i18n.js
+++ components/i18n.js
@@ -20,7 +20,7 @@ async function initializeI18n() {
     defLng = "all"; // Установка значения по умолчанию в случае ошибки
   }
 
-  const languages = ["au", "ca", "nz", "pl"];
+  const languages = ["au", "ca", "nz", "pl", "us", "se", 'fi'];
 
   // Используем метод map для сопоставления значений массива languages с defLng
   const matchedLanguages = languages.map((language) => {

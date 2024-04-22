b71c8a21e9152b49272798fda6b827fd2ccd3b43
Oleh <gend4016@gmail.com>
2024-04-22 12:06:44 +0300

    1044

--- app/fortune/page.tsx
+++ app/fortune/page.tsx
@@ -15,16 +15,16 @@ export default function Fortune() {
   const { t } = useTranslation();
   const banner = false;
 
-  const [usId, setUsId] = useState();
-  console.log("USID", usId);
+  // const [usId, setUsId] = useState();
+  // console.log("USID", usId);
 
   // Обновление размеров iframe при изменении размеров окна
   useEffect(() => {
     // Получение ID пользователя
-    if (typeof window !== "undefined") {
-      const userId = localStorage.getItem("user_id");
-      setUsId(userId);
-    }
+    // if (typeof window !== "undefined") {
+    //   const userId = localStorage.getItem("user_id");
+    //   setUsId(userId);
+    // }
     const updateIframeSize = () => {
       const screenWidth = window.innerWidth;
       setIframeWidth(screenWidth <= 767 ? "100%" : "1170px");
@@ -38,59 +38,59 @@ export default function Fortune() {
   }, []);
 
   // Функция для обнуления времени в объекте даты
-  const startOfDay = (date: Date) => {
-    date.setHours(0, 0, 0, 0);
-    return date;
-  };
+  // const startOfDay = (date: Date) => {
+  //   date.setHours(0, 0, 0, 0);
+  //   return date;
+  // };
 
   // Функция для обновления данных пользователя, если необходимо
-  const updateUserDataIfNeeded = async (data: UserData) => {
-    if (!data) return;
+  // const updateUserDataIfNeeded = async (data: UserData) => {
+  //   if (!data) return;
 
-    const oldDate = data.winbalance ? new Date(data.winbalance) : new Date(0);
-    startOfDay(oldDate);
-    const newDate = new Date();
-    startOfDay(newDate);
+  //   const oldDate = data.winbalance ? new Date(data.winbalance) : new Date(0);
+  //   startOfDay(oldDate);
+  //   const newDate = new Date();
+  //   startOfDay(newDate);
 
-    if (newDate.getTime() - oldDate.getTime() >= 86400000) {
-      const formattedNewDate = newDate.toISOString();
+  //   if (newDate.getTime() - oldDate.getTime() >= 86400000) {
+  //     const formattedNewDate = newDate.toISOString();
 
-      try {
-        const body = JSON.stringify({
-          id: usId,
-          winbalance: formattedNewDate,
-        });
-        const response = await fetch(
-          "https://pickbonus.myawardwallet.com/api/user/update_time.php",
-          {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },
-            body,
-          }
-        );
+  //     try {
+  //       const body = JSON.stringify({
+  //         id: usId,
+  //         winbalance: formattedNewDate,
+  //       });
+  //       const response = await fetch(
+  //         "https://pickbonus.myawardwallet.com/api/user/update_time.php",
+  //         {
+  //           method: "PUT",
+  //           headers: { "Content-Type": "application/json" },
+  //           body,
+  //         }
+  //       );
 
-        if (!response.ok) throw new Error(`Error: ${response.status}`);
-        console.log("Данные успешно обновлены");
-        await fetchData(); // Перезагружаем данные пользователя
-      } catch (error) {
-        console.error("Ошибка при обновлении данных пользователя:", error);
-      }
-    } else {
-      console.log("Обновление не требуется.");
-    }
-  };
+  //       if (!response.ok) throw new Error(`Error: ${response.status}`);
+  //       console.log("Данные успешно обновлены");
+  //       await fetchData(); // Перезагружаем данные пользователя
+  //     } catch (error) {
+  //       console.error("Ошибка при обновлении данных пользователя:", error);
+  //     }
+  //   } else {
+  //     console.log("Обновление не требуется.");
+  //   }
+  // };
 
   // // Функция для получения данных пользователя
-  const fetchData = async () => {
-    if (!usId) return;
-    try {
-      const data = await getUserData(usId);
-      setUserData(data);
-      console.log("DATA", data)
-    } catch (error) {
-      console.error("Ошибка при получении данных пользователя:", error);
-    }
-  };
+  // const fetchData = async () => {
+  //   if (!usId) return;
+  //   try {
+  //     const data = await getUserData(usId);
+  //     setUserData(data);
+  //     console.log("DATA", data)
+  //   } catch (error) {
+  //     console.error("Ошибка при получении данных пользователя:", error);
+  //   }
+  // };
 
   // addEventListener("message", (event) => {
   //   console.log("EVENT", event);
@@ -98,33 +98,33 @@ export default function Fortune() {
   // updateUserDataIfNeeded(userData);
   // });
 
-  const [messageData, setMessageData] = useState(null);
-  useEffect(() => {
-    const handleMessage = (event) => {
-      if (event.origin !== window.location.origin) {
-        console.error("unauthorized origin", event.origin);
-        return;
-      }
-      let dataString =
-        typeof event.data === "string"
-          ? event.data
-          : JSON.stringify(event.data);
+  // const [messageData, setMessageData] = useState(null);
+  // useEffect(() => {
+  //   const handleMessage = (event) => {
+  //     if (event.origin !== window.location.origin) {
+  //       console.error("unauthorized origin", event.origin);
+  //       return;
+  //     }
+  //     let dataString =
+  //       typeof event.data === "string"
+  //         ? event.data
+  //         : JSON.stringify(event.data);
 
-      if (dataString.includes("Json:")) {
-        setMessageData(dataString);
+  //     if (dataString.includes("Json:")) {
+  //       setMessageData(dataString);
 
-        fetchData();
-        if (userData) {
-          updateUserDataIfNeeded(userData);
-          // return;
-        }
-      }
-      console.log("Received data", dataString);
-    };
+  //       fetchData();
+  //       if (userData) {
+  //         updateUserDataIfNeeded(userData);
+  //         // return;
+  //       }
+  //     }
+  //     console.log("Received data", dataString);
+  //   };
 
-    window.addEventListener("message", handleMessage);
-    return () => window.removeEventListener("message", handleMessage);
-  }, [messageData]);
+  //   window.addEventListener("message", handleMessage);
+  //   return () => window.removeEventListener("message", handleMessage);
+  // }, [messageData]);
 
   const target = "target-fw-brands-wheel-page";
   return (
